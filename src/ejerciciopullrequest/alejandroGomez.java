ac1f7ccc457311178e91dd6c93d0a0cb6a28c8e2
alex <alejangomez27@gmail.com>
2017-01-19 09:08:45 +0100

    Añadida modificacion sobre pull request

---
+++ src/ejerciciopullrequest/alejandroGomez.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciopullrequest;
+
+/**
+ *
+ * @author alex
+ */
+public class alejandroGomez {
+    public void simulacion(){
+        System.out.println("Esto es una simulacion de un pull request.");
+    }
+}
